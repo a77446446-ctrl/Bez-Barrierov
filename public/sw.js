72de197f181886d396040dfbde9f60c7f96860d7
a77446446 <a77446446@gmail.com>
2026-03-09 14:40:03 +0300

    Nov3

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'bez-barrierov-v5';
+const CACHE_NAME = 'bez-barrierov-v10';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/app-icon.png'
+  '/logo.png'
 ];
 
 self.addEventListener('install', (event) => {
